ba72c65e98b5fdd3765c33cba1bded20daa58fbb
twobnanaday <twobnanaday@gmail.com>
2022-04-23 19:06:33 -0500

    walked through three examples of data binding, ended with demo of passing template variable to onClick() event and showing in alert box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'hello-world',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Angular ';
+  isSpecial: boolean = false;
+  paraColor: string = 'green';
+
+  onClick(x: string): void {
+    alert('The phone number is ' + x);
+  }
 }
